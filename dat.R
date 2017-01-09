f99c5af5ebb576cd0195356363d18bd53f6894d4
Martin Skarzynski <marskar@users.noreply.github.com>
2017-01-09 17:03:23 -0500

    Code to download data for final project

---
+++ dat.R
@@ -0,0 +1,5 @@
+download.file("http://www.journalofaccountancy.com/content/dam/jofa/issues/2017/jan/general-ledger-collins.xlsx", "dat.xlsx")
+### http://www.journalofaccountancy.com/issues/2017/jan/general-ledger-data-mining.html
+install.packages("rio")
+library(rio)
+convert(dat.csv, )
